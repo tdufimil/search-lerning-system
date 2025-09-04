af6fb6fd6619b34d6561f43b1ce00a73f5afa0b4
taku010 <22fi010@ms.dendai.ac.jp>
2025-09-04 14:30:30 +0900

    画面遷移後、上端から表示するように修正

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ import HashExplain from "./components/HashExplain"
 import LinearPractice from "./components/LinearPractice"
 import BinaryPractice from "./components/BinaryPractice"
 import HashPractice from "./components/HashPractice"
+import ScrollToTop  from './components/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/LinearExplain" element={<LinearExplain />} />
-        <Route path="/BinaryExplain" element={<BinaryExplain />} />
-        <Route path="/HashExplain" element={<HashExplain />} />
-        <Route path="/LinearPractice" element={<LinearPractice />} />
-        <Route path="/BinaryPractice" element={<BinaryPractice />} />
-        <Route path="/HashPractice" element={<HashPractice />} />
-      </Routes>
+      <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/LinearExplain" element={<LinearExplain />} />
+          <Route path="/BinaryExplain" element={<BinaryExplain />} />
+          <Route path="/HashExplain" element={<HashExplain />} />
+          <Route path="/LinearPractice" element={<LinearPractice />} />
+          <Route path="/BinaryPractice" element={<BinaryPractice />} />
+          <Route path="/HashPractice" element={<HashPractice />} />
+        </Routes>
     </BrowserRouter>
   );
 }
