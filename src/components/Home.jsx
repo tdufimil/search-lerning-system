1f92ffd01e511fa8b18c9b1139d08e1f65fd4c0d
taku010 <22fi010@ms.dendai.ac.jp>
2026-02-01 11:50:36 +0900

    ホーム修正

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -78,29 +78,7 @@ function Home() {
            </div>
           </div>   
         </div>
-        <div className="research">
-          <h3>アンケート</h3>
-          <p className="researchtxt">以下のリンクからアンケートの回答をお願いします。</p>
-          {mode === 'a' ? (
-          <>
-            <p>線形探索</p>
-            <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=ixIOAf0kL06CbvxAt5p-YWUrbDyN8qtLmjvYfIJZ-g9UQk9NMzVBUDc0VFQ4VFhYM1AwR1NMNkZRUC4u">https://forms.office.com/r/wB2Zc0Lpim</a>
-            <p>二分探索</p>
-            <a href="https://forms.office.com/r/gSBi260KRr">https://forms.office.com/r/gSBi260KRr</a>
-            <p>ハッシュ法</p>
-            <a href="https://forms.office.com/r/m8ihnxJ2Hm">https://forms.office.com/r/m8ihnxJ2Hm</a>
-          </>
-          ):(
-          <>
-            <p>線形探索</p>
-            <a href="https://forms.office.com/r/63nexUHGBC">https://forms.office.com/r/63nexUHGBC</a>
-            <p>二分探索</p>
-            <a href="https://forms.office.com/r/V4EmwqjAjN">https://forms.office.com/r/V4EmwqjAjN</a>
-            <p>ハッシュ法</p>
-            <a href="https://forms.office.com/r/htQbTkzypj">https://forms.office.com/r/htQbTkzypj</a>
-          </>
-          )}
-        </div>
+       
       </div>
     </div>
   )
